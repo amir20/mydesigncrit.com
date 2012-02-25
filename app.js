19ff37f6c3cf4f4f90a70d2d77643fb73618bfdd
Amir Raminfar <amir.raminfar@opower.com>
2012-02-25 01:18:14 -0500

    Add mongo and clean up

--- app.js
+++ app.js
@@ -4,7 +4,10 @@
  */
 
 var express = require('express')
-  , routes = require('./routes');
+  , coffee = require("coffee-script")
+  , mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/reviewme');
 
 var app = module.exports = express.createServer();
 
@@ -28,10 +31,7 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
-// Routes
-
-app.get('/', routes.index);
-app.post('/', routes.take);
+require('./routes')(app);
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
