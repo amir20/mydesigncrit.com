67586e87a9a1f0dc022a6aca4ae99f30eacc6600
Amir Raminfar <findamir@gmail.com>
2013-12-01 19:23:48 -0800

    Fixds xsrf token for ng

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  # protect_from_forgery with: :null_session
+  protect_from_forgery with: :null_session
+  after_filter :set_csrf_cookie_for_ng
 
   def current_user
     user = super
@@ -10,5 +11,14 @@ class ApplicationController < ActionController::Base
       sign_in(:user, user)
     end
     user
+  end
+
+  protected
+  def verified_request?
+    super || form_authenticity_token == request.headers['X-XSRF-TOKEN']
+  end
+
+  def set_csrf_cookie_for_ng
+    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
   end
 end
