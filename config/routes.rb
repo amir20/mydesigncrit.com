b28ada2a4c455e604e3b870dac19a2f1b651dad1
Amir Raminfar <findamir@gmail.com>
2013-12-07 09:32:00 -0800

    Adds preview button and cleans up routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,13 @@ DesigncritIo::Application.routes.draw do
     get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   end
 
-  resources :projects do
-    resources :pages do
-      resources :crits
+  resources :projects, except: [:edit, :new] do
+    resources :pages, except: [:edit, :new] do
+      resources :crits, except: [:edit, :new]
     end
   end
 
-  get '/v/:id', to: 'projects#share'
+  get '/v/:id', to: 'projects#share', as: :share
 
   root 'welcome#index'
   get '/delayed_job' => DelayedJobWeb, :anchor => false
