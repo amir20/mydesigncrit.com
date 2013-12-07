efca0ccf81ccc63610126ab66b2f8eed872b92b4
Amir Raminfar <findamir@gmail.com>
2013-12-06 22:44:19 -0800

    Adds user registration and copy

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,17 +5,31 @@ class User < ActiveRecord::Base
   def self.find_for_facebook_oauth(auth, guest_user)
     user = User.where(email: auth.info.email).first
 
-    # if user is already anonymous
+    if user
+      guest_user.projects.update_all(user_id: user.id)
+      guest_user.delete
+    else
+      guest_user.update(name: auth.extra.raw_info.name, email: auth.info.email, provider: 'facebook')
+      user = guest_user.becomes!(User)
+      user.save!
+    end
 
-
-    user = User.create(name: auth.extra.raw_info.name, email: auth.info.email, provider: 'facebook') unless user
     user
   end
 
   def self.find_for_google_oauth2(access_token, guest_user)
     data = access_token.info
     user = User.where(email: data['email']).first
-    user = User.create(name: data['name'], email: data['email'], provider: 'google') unless user
+
+    if user
+      guest_user.projects.update_all(user_id: user.id)
+      guest_user.delete
+    else
+      guest_user.update(name: data['name'], email: data['email'], provider: 'google')
+      user = guest_user.becomes!(User)
+      user.save!
+    end
+
     user
   end
 
