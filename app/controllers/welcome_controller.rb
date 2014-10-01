f23e18d68d88c4029bcf0df5cfc64c62d4e80200
Amir Raminfar <amir.raminfar@opower.com>
2014-09-30 17:38:16 -0700

    Adds features

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,6 @@ class WelcomeController < ApplicationController
   before_action :verify_authenticity_token
 
   def index
+    @projects = Project.accessible_by(current_ability).reorder(crits_count: :desc, updated_at: :desc)
   end
 end
