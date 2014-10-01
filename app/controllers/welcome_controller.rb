ae0dedd5a6ea5e1c4fae4abe45af1424d90b1f90
Amir Raminfar <amir.raminfar@opower.com>
2014-09-30 18:06:18 -0700

    Adds features projects with over color

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,6 @@ class WelcomeController < ApplicationController
   before_action :verify_authenticity_token
 
   def index
-    @projects = Project.accessible_by(current_ability).reorder(crits_count: :desc, updated_at: :desc)
+    @projects = Project.accessible_by(current_ability).reorder(crits_count: :desc, updated_at: :desc).limit(8)
   end
 end
